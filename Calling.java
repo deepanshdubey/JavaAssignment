38ec7dec18e31bf08358d70ada0b519f432ad9a2
Deepansh Dubey <56029566+deepanshdubey@users.noreply.github.com>
2021-09-23 21:08:17 +0530

    Assignment2

--- Calling.java
+++ Calling.java
@@ -1,26 +1,28 @@
 import java.io.*;
 class Calling
 {
+    static int k;
     void byValue(int a)
     {
         a=a*10;
         System.out.println("Call by Value = " + a);
     }
-    int byReference(int b)
+    int byReference(Calling refer)
     {
-        b=b+10;
-        return b;
+        refer.k=+10;
+        return k;
     }
     public static void main(String args[])throws IOException
     {
         BufferedReader br=new BufferedReader (new InputStreamReader(System.in));
-        int x,k;
+        int x;
         System.out.println("Please enter the number");
         x=Integer.parseInt(br.readLine());
         Calling ob= new Calling();
         ob.byValue(x);
-        k=ob.byReference(x);
+        ob.k=2000;
         System.out.println("Call by Reference = " + k);
 
     }
 }
+
