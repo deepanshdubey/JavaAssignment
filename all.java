8805691f2b94ac7b7f8fb929740154f096aec28b
fangs-d <72966722+fangs-d@users.noreply.github.com>
2022-10-30 21:18:47 +0530

    Update all.java

--- all.java
+++ all.java
@@ -222,3 +222,26 @@ class stacks
     System.out.println("Is Stack Empty: " + st.empty());
   }
 }
+
+import java.io.*;
+import java.util.*;
+
+class duplicateBrackets
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack <Integer> st = new Stack<>();
+
+        size = st.length();
+
+        for(i=0;i<size;++i)
+        {
+            if(st(i)=='(')
+                st.push('(');
+            if(st(i)==')')
+                st.pop(')');
+        }
+
+    }
+}
